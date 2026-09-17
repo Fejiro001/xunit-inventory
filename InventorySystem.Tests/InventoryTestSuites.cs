58af4dc627c0fbf76ea9bf7450b7fd87a8994cb1
Fejiro001 <aberefejiro@gmail.com>
2026-09-17 11:13:30 -0500

    test: add unit test for adding a valid product successfully

---
+++ InventorySystem.Tests/InventoryTestSuites.cs
@@ -0,0 +1,28 @@
+﻿namespace InventorySystem.Tests
+{
+    public class InventoryTestSuites
+    {
+        private readonly InventoryOrderService _orderService = new();
+
+        // === Happy Path ===
+        [Fact]
+        public void AddProduct_ValidProduct_SuccessfullySavesToInventory()
+        {
+            // Arrange
+            Product product = new Product
+            {
+                Id = "P500",
+                Name = "Sony Headphones",
+                UnitPrice = 350.00m,
+                StockQuantity = 10
+            };
+            // Act
+            _orderService.AddProduct(product);
+
+            // Assert
+            var productExists = _orderService.GetProduct(product.Id);
+            Assert.NotNull(productExists);
+            Assert.Equal("P500", productExists.Id);
+        }
+    }
+}
